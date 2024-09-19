2b1d9b5494fad976ea7acc466fcbf470a4390694
Stardusnt <bintangsava05@gmail.com>
2024-09-19 18:34:47 +0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] agrs) {
+        System.out.println("Nama:Bintang Sava Ozora P!");
+		System.out.println("NIM:607062400095");
+		System.out.println("Kelas:D3IF4801");
+    }
+}
